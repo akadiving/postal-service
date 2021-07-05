376e8b2b46c3c9e5a5ff7f603c9603da00b629d1
ako <akoivanishvili6@gmail.com>
2021-07-05 08:39:17 +0400

    production test

---
+++ dist/precache-manifest.75ac156c9ab315e21146f66800064d7f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d18a722f1a6f004f23cc",
+    "url": "/css/app.73f854ca.css"
+  },
+  {
+    "revision": "233b79b0d982ded576cf",
+    "url": "/css/chunk-vendors.02e05e5d.css"
+  },
+  {
+    "revision": "4b607f2c49f9104a89c894238fb655f9",
+    "url": "/img/list-svgrepo-com.4b607f2c.svg"
+  },
+  {
+    "revision": "f965f4d05122f683265afa5a5304f559",
+    "url": "/img/package-box-svgrepo-com.f965f4d0.svg"
+  },
+  {
+    "revision": "3367626c87662c75aa529c6c5772cbfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d18a722f1a6f004f23cc",
+    "url": "/js/app.56a7fe1c.js"
+  },
+  {
+    "revision": "233b79b0d982ded576cf",
+    "url": "/js/chunk-vendors.90159021.js"
+  },
+  {
+    "revision": "4b14c64efaf846819b9a229b4193c8b7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
